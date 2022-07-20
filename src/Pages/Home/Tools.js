60d2529472833d5ef970b37682d99d5a08ab5077
MD-Jubayer-Khan <mdjubayerkhan001@gmail.com>
2022-07-20 18:01:42 +0600

    create tool section

--- src/Pages/Home/Tools.js
+++ src/Pages/Home/Tools.js
@@ -1,43 +1,43 @@
-// import React from 'react';
-// import Tool from './Tool';
+import React from 'react';
+import Tool from './Tool';
 
-// const Tools = () => {
-//     const tools = [
-//         {
-//             _id: 1,
-//             name: 'Fluoride Treatment',
-//             description: '',
+const Tools = () => {
+    const tools = [
+        {
+            _id: 1,
+            name: 'Fluoride Treatment',
+            description: '',
          
-//         },
-//         {
-//             _id: 2,
-//             name: 'Cavity Filling',
-//             description: '',
+        },
+        {
+            _id: 2,
+            name: 'Cavity Filling',
+            description: '',
            
-//         },
-//         {
-//             _id: 3,
-//             name: 'Teeth Whitening',
-//             description: '',
+        },
+        {
+            _id: 3,
+            name: 'Teeth Whitening',
+            description: '',
            
-//         }]
+        }]
 
-//     return (
-//         <div className='my-28'>
-//         <div className='text-center'>
-//             <h3 className='text-primary  text-xl font-bold uppercase'>Our tools</h3>
-//             <h2 className='text-4xl'>tools We Provide</h2>
-//         </div>
-//         <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10'>
-//             {
-//                 tools.map(tool =><Tool
-//                     key={tool._id}
-//                     tool={tool}
-//                 ></Tool>)
-//             }
-//         </div>
-//     </div>
-//     );
-// };
+    return (
+        <div className='my-28'>
+        <div className='text-center'>
+            <h3 className='text-primary  text-xl font-bold uppercase'>Our tools</h3>
+            <h2 className='text-4xl'>tools We Provide</h2>
+        </div>
+        <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10'>
+            {
+                tools.map(tool =><Tool
+                    key={tool._id}
+                    tool={tool}
+                ></Tool>)
+            }
+        </div>
+    </div>
+    );
+};
 
-// export default Tools;
+export default Tools;
