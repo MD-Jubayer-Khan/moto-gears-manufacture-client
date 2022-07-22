cb4963c015666e486b614fcd0b4d5ec1580a6d87
MD-Jubayer-Khan <mdjubayerkhan001@gmail.com>
2022-07-22 12:05:55 +0600

    created protected route, handle redirect login page after reload

--- src/UserAuth/RequireAuth.js
+++ src/UserAuth/RequireAuth.js
@@ -1,21 +1,21 @@
-// import React from 'react';
-// import { useAuthState } from 'react-firebase-hooks/auth';
-// import { Navigate, useLocation } from 'react-router-dom';
-// import auth from '../firebase.init';
+import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { Navigate, useLocation } from 'react-router-dom';
+import auth from '../firebase.init';
 
-// const RequireAuth = ({children}) => {
-//     const [user, loading] = useAuthState(auth);
-//     const location = useLocation();
+const RequireAuth = ({children}) => {
+    const [user, loading] = useAuthState(auth);
+    const location = useLocation();
 
-//     if(loading) {
-//         return <p>Loading...</p>
-//     }
+    if(loading) {
+        return <p>Loading...</p>
+    }
 
-//     if(!user){
-//         return <Navigate to="/login" state={{ from: location}} replace></Navigate>
-//     }
+    if(!user){
+        return <Navigate to="/login" state={{ from: location}} replace></Navigate>
+    }
 
-//     return children;
-// };
+    return children;
+};
 
-// export default RequireAuth;
+export default RequireAuth;
