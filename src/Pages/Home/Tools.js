29b976d5883cc80187ca57ff29fb4ee54d87cdf8
MD-Jubayer-Khan <mdjubayerkhan001@gmail.com>
2022-07-22 23:03:09 +0600

    display parts data from database

--- src/Pages/Home/Tools.js
+++ src/Pages/Home/Tools.js
@@ -1,36 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+// import { useQuery } from 'react-query';
 import Tool from './Tool';
 
 const Tools = () => {
-    const tools = [
-        {
-          "_id": "124e13b9-2d54-4b2f-a74d-a77b362d6ead",
-          "name": "CNC key",
-          "text": "For YAMAHA NMAX 150 2015-2019. For YAMAHA NMAX 125 2015-2019. Product material: CNC aluminum alloy. Reminder: This is only a key cover, does not contain the ...",
-          "price": 699,
-          "qty":100,
-          "availableQty": 351,
-          "img": "https://ae01.alicdn.com/kf/Hce357a18942243b4a8752fc36908cb75T/Universal-Motorcycle-Accessories-CNC-Key-Cap-Creative-Product-Key-Case-Auto-Accessories-for-Yamaha-Fzs-Fz600.jpg_Q90.jpg_.webp"
-        },
-        {
-            "_id": "4b2f-a74d-a77b362d6ead",
-            "name": "Front shock absorber",
-            "text": "Motorcycle shock absorber compatible with most vehicles. Easy to install and remove. Made of superior quality iron, waterproof, shockproof, ...",
-            "price": 799,
-            "qty":100,
-            "availableQty": 751,
-            "img": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_ejRhqM7KYNc9INIbsCrbzQ4U-FbqyGjuUQ&usqp=CAU"
-          },
-          {
-            "_id": "124e13b9-2d54-4b2f",
-            "name": "Protector Kit",
-            "text": "Our complete DIY Motorcycle Kits are perfect for any garage Custom Motorcycle Built Project. We specialize in creating one off bikes, built for the rider.",
-            "price": 1500,
-            "qty":100,
-            "availableQty": 551,
-            "img": "https://ae01.alicdn.com/kf/HTB1.c3MOVXXXXb6aFXXq6xXFXXXa/Injection-ABS-Complete-Fairings-For-BMW-S1000RR-2010-Year-10-ABS-Plastic-Motorcycle-Fairing-Kit-White.jpg"
-          }
-    ]
+    // const { data: tools, isLoading } = useQuery('allTools', () => fetch('http://localhost:5000/parts')
+    // .then(res => res.json()))
+
+    // if(isLoading){
+    //     return <p>Loading...</p>
+    // }
+    const [tools, setTools] = useState([]);
+
+    useEffect(()=>{
+        fetch('http://localhost:5000/parts')
+        .then(res => res.json())
+        .then(data => setTools(data))
+    },[])
 
     return (
         <div className='my-28 mx-12'>
@@ -40,7 +25,7 @@ const Tools = () => {
         </div>
         <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10'>
             {
-                tools.map(tool =><Tool
+                tools?.map(tool =><Tool
                     key={tool._id}
                     tool={tool}
                 ></Tool>)
--- src/index.js
+++ src/index.js
@@ -4,14 +4,22 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import {
+  QueryClient,
+  QueryClientProvider,
+} from 'react-query';
+
+const queryClient = new QueryClient()
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+    </QueryClientProvider>
   </React.StrictMode>
- </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
