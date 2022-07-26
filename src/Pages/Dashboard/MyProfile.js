7d5a7b728354638377d20ee3411e0c1d09016f1f
MD-Jubayer-Khan <mdjubayerkhan001@gmail.com>
2022-07-26 16:41:13 +0600

    added conditional rendaring feature for admin

--- src/Pages/Dashboard/MyProfile.js
+++ src/Pages/Dashboard/MyProfile.js
@@ -1,11 +1,58 @@
-import React from 'react';
+// import React from 'react';
+// import { useAuthState } from 'react-firebase-hooks/auth';
+// import auth from '../../firebase.init';
+// import img from '../../Assets/img/notFound.jpg'
+// import { toast } from 'react-toastify';
+// import { useForm } from 'react-hook-form';
 
-const MyProfile = () => {
-    return (
-        <div>
-            <h1>This is my profile page</h1>
-        </div>
-    );
-};
+// const MyProfile = () => {
+//     const [user] = useAuthState(auth)
 
-export default MyProfile;
+//     const { register, handleSubmit,  reset } = useForm(); 
+    
+//     const onSubmit = data => {
+//         fetch('https://limitless-fjord-63635.herokuapp.com/parts', {
+//             method: 'POST',
+//             headers: {
+//                 'content-type': 'application/json'
+//             },
+//             body: JSON.stringify(data)
+//         })
+//         .then(res=> res.json())
+//         .then(result =>{
+//             if(result.success){
+//                 toast.success('Product added successfully')
+//             }
+//         } )
+//         reset()
+//     };
+
+//     return (
+//         <div className='flex'>
+//         <div class="artboard phone-3 bg-base-300 mt-2">           
+//         <div class="avatar mt-4">
+//             <div class="w-24 rounded-full">
+//              <img src={user?.photoURL || img} alt="profile" />
+//             </div>
+//          </div>
+//             <p>Name: {user.displayName}</p>
+//         </div>
+
+//             <div className='m-4'>
+//             <div className='card lg:max-w-lg bg-base-100 shadow-xl mt-20 mx-auto'>
+//             <form onSubmit={handleSubmit(onSubmit)}>
+//                 <input className="input input-bordered w-full max-w-xs" placeholder='Name' {...register("name", { required: true, })} />
+//                 <textarea className="input input-bordered w-full max-w-xs my-4" placeholder='describe' {...register("text")} />
+//                 <input className="input input-bordered w-full max-w-xs" placeholder='Price' type="number" {...register("price")} />
+//                 <input className="input input-bordered w-full max-w-xs my-4" placeholder='Minimum Quantity should 100' type="number" {...register("qty")}  />
+//                 <input className="input input-bordered w-full max-w-xs my-4" placeholder='Total Quantity' type="number" {...register("availableQty")} />
+//                 <input className="input input-bordered w-full max-w-xs mb-4" placeholder='Photo URL' type="text" {...register("img")} />
+//                 <input type="submit" value="Add Product"  className="input input-bordered w-full max-w-xs btn bg-gradient-to-r from-secondary to-primary mb-8"/>
+//             </form>
+//         </div>
+//             </div>
+//         </div>
+//     );
+// };
+
+// export default MyProfile;
--- src/Pages/Dashboard/OrderRow.js
+++ src/Pages/Dashboard/OrderRow.js
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const OrderRow = ({orders, index}) => {
     const {Parts, qty, user }= orders;
+   
+    const ship = <p className='btn btn-xs btn-success text-white'>Shipped</p>
+    const pending = <p className='btn btn-xs btn-error text-white'>Pending</p>
+    const [shipped, setShippied] = useState(pending)
+    
+    const handleShipped =() =>{
+        setShippied(ship)
+    }
 
     return (
         <tr>
@@ -9,7 +17,7 @@ const OrderRow = ({orders, index}) => {
             <td><span className='font-bold'>{Parts}</span> <br /> <small className='text-error-content'>{user}</small></td>
             <td className='font-bold'>{qty}</td>
         <td>
-            <button class="btn btn-xs btn-error">pending</button>
+            <button onClick={handleShipped} >{shipped}</button>
         </td>
         </tr>
     );
